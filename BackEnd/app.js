376e0968157df0cb9934199663cd7b4b45b177c2
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2024-12-15 03:15:04 +0700

    add a backend file

---
+++ BackEnd/app.js
@@ -0,0 +1,76 @@
+const express = require("express")
+const port = 8080
+const mongoose = require("mongoose")
+const app = express()
+
+app.use(express.json())
+mongoose.connect("mongodb://localhost:27017/PhakapholDB").then(()=>{
+    console.log("Connect MongoDb Success")
+}).catch(err =>{
+    console.log("Cant connect to MongoDb")
+    throw err
+})
+
+const NewSchema = new mongoose.Schema(
+    {
+        username : String,
+        password : String,
+        buyinglist: Array
+    }
+)
+const PkShopDB = mongoose.model("PkShopDB",NewSchema)
+
+app.post("/pkshop",async(req,res)=>{
+    try{
+        const userinp = req.body
+        const user = new PkShopDB({
+            username : userinp.username,
+            password : userinp.password,
+        });
+        await user.save();
+        res.status(201).json({
+            message:"Save Successfull",
+            username: userinp.username,
+            password : userinp.password.length
+        })
+    }catch(err){
+        res.status(500).json({
+            message: "Error "
+        }
+        )
+    
+    
+    }
+    
+})
+app.get("/pkshop", async (req, res) => {
+    try {
+        const alluserdata = await PkShopDB.find();
+        res.status(200).json(alluserdata);
+    } catch (err) {
+        console.error("Error fetching data:", err);
+        res.status(500).json({
+            message: "Error occurred while fetching user data"
+        });
+    }
+});app.listen(port,async()=>{
+    console.log("App listening to port 8080")
+})
+
+app.get("/pkshop/:username",async(req,res)=>{
+    try{
+        const userparamsfind = await PkShopDB.findOne({username: req.params.username})
+        if (userparamsfind){
+            res.send("Login Success")
+        }
+        else{
+            res.send("user is not in the database")
+        }
+    }
+    catch(err){
+        throw err
+    }
+})
+app.get("/",(req,res)=>{
+    res.send("Connected")
+})
