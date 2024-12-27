142c0da8b91be92b49c76135b4d9b344c386b92e
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2024-12-27 16:51:58 +0700

    add a length to nav

--- FrontEnd/src/components/Nav.jsx
+++ FrontEnd/src/components/Nav.jsx
@@ -6,7 +6,18 @@ import "./components.css";
 import userprofile from "../assets/profile.png";
 
 function Nav() {
-  const [count, setCount] = useState(0);
+  const [cartItems, setCartItems] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8777/cart")
+      .then((response) => {
+        setCartItems(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching cart items:", error);
+      });
+  }, []);
+  const totalItems = cartItems.length;
 
  
   return (
@@ -37,7 +48,9 @@ function Nav() {
                       strokeWidth="2"
                       d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                     />
+                    
                   </svg>
+                  <span className="badge badge-sm indicator-item">{totalItems}</span>
                 </div>
               </div>
             </a>
